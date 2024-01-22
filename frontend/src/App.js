b4149df798142b9213bf41e4fb2fa4fcf384920e
Daniil Kiselev <danqkiselev@yandex.ru>
2024-01-22 12:28:07 +0300

    Initial React App

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <header className="app-header">
+                <h1>Book Library</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
